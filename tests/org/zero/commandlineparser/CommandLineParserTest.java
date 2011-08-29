e3ce5e6074de6317b39d1451945aa7cffa38f6ce
rmonico <rmonico1@gmail.com>
2011-08-29 15:35:34 -0300

    Fontes preexistentes.

---
+++ tests/org/zero/commandlineparser/CommandLineParserTest.java
@@ -0,0 +1,159 @@
+package org.zero.commandlineparser;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommandLineParserTest {
+
+	CommandLineParser parser;
+
+	@Before
+	public void setUp() throws Exception {
+		parser = new CommandLineParser();
+	}
+
+	@Test
+	public void testMinimalParsing() {
+		parser.setCommandLine(new String[] { "arg1", "arg1_value" });
+
+		BasicSwitch switches = new BasicSwitch();
+
+		parser.setSwitchesObject(switches);
+
+		parser.doParsing();
+
+		assertEquals("arg1", "arg1_value", switches.getArg1());
+	}
+
+	@Test
+	public void testNamedSwitches() {
+		parser.setCommandLine(new String[] { "argument1", "argument1_value" });
+
+		NamedSwitch switches = new NamedSwitch();
+
+		parser.setSwitchesObject(switches);
+
+		parser.doParsing();
+
+		assertEquals("argument1", "argument1_value", switches.getArg1());
+	}
+
+	@Test
+	public void testDefaultValueSwitch() {
+		parser.setCommandLine(new String[] {});
+
+		DefaultValueSwitch switches = new DefaultValueSwitch();
+
+		parser.setSwitchesObject(switches);
+
+		parser.doParsing();
+
+		assertEquals("arg1", "argument1_value", switches.getArg1());
+	}
+	
+	@Test
+	public void testParsedSwitch() {
+		parser.setCommandLine(new String[] {"arg1", "45"});
+
+		parser.addParser("arg1parser", new IntegerParser());
+		
+		ParsedSwitch switches = new ParsedSwitch();
+		
+		parser.setSwitchesObject(switches);
+		
+		parser.doParsing();
+		
+		assertEquals("arg1", (int) 45, (int) switches.getArg1());
+	}
+
+	public void testDefaultSwitch() {
+		parser.setCommandLine(new String[] { "help" });
+
+		DefaultSwitch switches = new DefaultSwitch();
+		
+		switches.setShowHelp(false);
+
+		parser.setSwitchesObject(switches);
+
+		parser.doParsing();
+
+		assertEquals("show help", true, switches.getShowHelp());
+	}
+}
+
+class BasicSwitch {
+
+	private String arg1;
+
+	@CommandLineSwitch
+	public void setArg1(String value) {
+		arg1 = value;
+	}
+
+	public String getArg1() {
+		return arg1;
+	}
+
+}
+
+class NamedSwitch {
+
+	private String arg1;
+
+	@CommandLineSwitch(param = "argument1")
+	public void setArg1(String value) {
+		arg1 = value;
+	}
+
+	public String getArg1() {
+		return arg1;
+	}
+
+}
+
+class DefaultValueSwitch {
+	private String arg1;
+
+	@CommandLineSwitch(defaultValue = "argument1_value")
+	public void setArg1(String arg1) {
+		this.arg1 = arg1;
+	}
+
+	public String getArg1() {
+		return arg1;
+	}
+}
+
+class IntegerParser {
+	public Integer parse(String value) {
+		return Integer.parseInt(value);
+	}
+}
+
+class ParsedSwitch {
+	private Integer arg1;
+
+	@CommandLineSwitch(parser="arg1parser.parse")
+	public void setArg1(Integer value) {
+		arg1 = value;
+	}
+	
+	public Integer getArg1() {
+		return arg1;
+	}
+}
+
+class DefaultSwitch  {
+	private boolean showHelp;
+	
+	@CommandLineSwitch(param="help")
+	public void setShowHelp(boolean value) {
+		showHelp = value;
+	}
+	
+	public boolean getShowHelp() {
+		return showHelp;
+	}
+}
