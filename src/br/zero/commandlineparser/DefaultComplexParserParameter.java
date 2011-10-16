152d80f8f477142aed40cd6956751eaf266c7421
rmonico <rmonico1@gmail.com>
2011-10-15 22:54:54 -0300

    Implementação padrão de ComplexParserParameter

---
+++ src/br/zero/commandlineparser/DefaultComplexParserParameter.java
@@ -0,0 +1,26 @@
+package br.zero.commandlineparser;
+
+import java.util.Map;
+
+class DefaultComplexParserParameter implements ComplexParserParameter {
+
+	private String[] args;
+	private Map<String, Object> parsers;
+
+	@Override
+	public String[] getArgs() {
+		return args;
+	}
+
+	public void setArgs(String[] value) {
+		args = value;
+	}
+
+//	@Override
+//	public Map<String, Object> getParsers() {
+//		return parsers;
+//	}
+//
+//	public void setParsers() {
+//	}
+}
