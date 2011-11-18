61122118d7173aa59b9b9975af3e32be39d7b44e
rmonico <rmonico1@gmail.com>
2011-11-18 03:51:35 -0200

    Novo parser para tipos double

--- src/br/zero/commandlineparser/parsers/PrimitiveParsers.java
+++ src/br/zero/commandlineparser/parsers/PrimitiveParsers.java
@@ -15,6 +15,16 @@ public class PrimitiveParsers {
 		}
 	}
 	
+	@CommandLineArgumentParserMethod(messageMethod="getError")
+	public double parseDouble(String value) {
+		try {
+			return Double.parseDouble(value);
+		} catch (NumberFormatException nfe) {
+			error = "Invalid number format (\"" + value + "\").";
+			return 0.0;
+		}
+	}
+
 	public String getError() {
 		return error;
 	}
