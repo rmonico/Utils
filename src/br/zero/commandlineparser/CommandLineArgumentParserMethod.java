0f464b28176383cfd20838f2ef3e2c6a573e7150
rmonico <rmonico1@gmail.com>
2011-11-19 20:20:05 -0200

    Novo item TODO

--- src/br/zero/commandlineparser/CommandLineArgumentParserMethod.java
+++ src/br/zero/commandlineparser/CommandLineArgumentParserMethod.java
@@ -7,6 +7,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
+// TODO Métodos marcados com esta anotação devem poder lançar ParserException opcionalmente
 public @interface CommandLineArgumentParserMethod {
 
 	String messageMethod() default "";
