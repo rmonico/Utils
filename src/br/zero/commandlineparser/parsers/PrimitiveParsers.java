478a4db6fd3528814067ff0c1e692b5362603f50
rmonico <rmonico1@gmail.com>
2011-11-21 11:22:49 -0200

    Método parseInteger suportando valores nulos.

--- src/br/zero/commandlineparser/parsers/PrimitiveParsers.java
+++ src/br/zero/commandlineparser/parsers/PrimitiveParsers.java
@@ -3,10 +3,16 @@ package br.zero.commandlineparser.parsers;
 import br.zero.commandlineparser.CommandLineArgumentParserMethod;
 
 public class PrimitiveParsers {
-	private String error = null;
+	private String error;
 	
 	@CommandLineArgumentParserMethod(messageMethod="getError")
 	public Integer parseInteger(String value) {
+		error = null;
+		
+		if ("null".equals(value)) {
+			return null;
+		}
+		
 		try {
 			return Integer.parseInt(value);
 		} catch (NumberFormatException nfe) {
@@ -17,6 +23,8 @@ public class PrimitiveParsers {
 	
 	@CommandLineArgumentParserMethod(messageMethod="getError")
 	public double parseDouble(String value) {
+		error = null;
+		
 		try {
 			return Double.parseDouble(value);
 		} catch (NumberFormatException nfe) {
