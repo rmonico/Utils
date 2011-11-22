8cb5d2ecd6b4b61f45053ea8068de2291af39e9e
rmonico <rmonico1@gmail.com>
2011-11-22 18:31:36 -0200

    Codificação preliminar dos métodos getAlignedString em ColumnAlignment

--- src/br/zero/textgrid/ColumnAlignment.java
+++ src/br/zero/textgrid/ColumnAlignment.java
@@ -3,22 +3,74 @@ package br.zero.textgrid;
 // Não usei enums aqui para permitir extensões da classe
 public abstract class ColumnAlignment {
 
-	public abstract StringBuilder getAlignedString(Integer integer, StringBuilder value);
+	public abstract StringBuilder getAlignedString(Integer finalLength, StringBuilder value);
+	
+	private static StringBuilder replicateChar(char ch, int count) {
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i=0; i<count; i++) {
+			sb.append(ch);
+		}
+		
+		return sb;
+	}
 
 	public static final ColumnAlignment LEFT = new ColumnAlignment() {
 		@Override
-		public StringBuilder getAlignedString(Integer integer, StringBuilder value) {
-			return null;
+		public StringBuilder getAlignedString(Integer finalLength, StringBuilder value) {
+			StringBuilder returnValue;
+			
+			int valueLength = value.length();
+			
+			if (valueLength < finalLength) {
+				StringBuilder spaces = new StringBuilder(replicateChar(' ', finalLength - valueLength));
+				
+				returnValue = new StringBuilder(value);
+				
+				returnValue.append(spaces);
+				
+			} else if (valueLength > finalLength) {
+				returnValue = new StringBuilder(value);
+				
+				returnValue.delete(finalLength-3, returnValue.length());
+				returnValue.append("...");
+			} else {
+				// valueLength == finalLength, nada a fazer
+				returnValue = value;
+			}
+			
+			return returnValue;
 		}
 	};
 	public static final ColumnAlignment CENTER = new ColumnAlignment() {
-		public StringBuilder getAlignedString(Integer integer, StringBuilder value) {
+		public StringBuilder getAlignedString(Integer finalLength, StringBuilder value) {
 			return null;
 		};
 	};
 	public static final ColumnAlignment RIGHT = new ColumnAlignment() {
-		public StringBuilder getAlignedString(Integer integer, StringBuilder value) {
-			return null;
+		public StringBuilder getAlignedString(Integer finalLength, StringBuilder value) {
+			StringBuilder returnValue;
+			
+			int valueLength = value.length();
+			
+			if (valueLength < finalLength) {
+				StringBuilder spaces = new StringBuilder(replicateChar(' ', finalLength - valueLength));
+				
+				returnValue = new StringBuilder(spaces);
+				
+				returnValue.append(value);
+				
+			} else if (valueLength > finalLength) {
+				returnValue = new StringBuilder(value);
+				
+				returnValue.insert(0, "...");
+				returnValue.delete(finalLength-3, returnValue.length());
+			} else {
+				// valueLength == finalLength, nada a fazer
+				returnValue = value;
+			}
+			
+			return returnValue;
 		};
 	};
 
