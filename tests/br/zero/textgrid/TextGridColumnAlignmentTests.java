57d8a53e90a386339199ddc1d4b6e917b4e8b0c5
rmonico <rmonico1@gmail.com>
2011-11-22 18:44:34 -0200

    Testes da classe TextGridColumnAlignment

---
+++ tests/br/zero/textgrid/TextGridColumnAlignmentTests.java
@@ -0,0 +1,93 @@
+package br.zero.textgrid;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TextGridColumnAlignmentTests {
+	
+	@Test
+	public void doLeftAlignmentTest() {
+		StringBuilder alignedValue = TextGridColumnAlignment.LEFT.getAlignedString(10, new StringBuilder("abcdef"));
+		
+		assertEquals("Left alignment", "abcdef    ", alignedValue);
+	}
+	
+	@Test
+	public void doLeftAlignmentTest2() {
+		StringBuilder alignedValue = TextGridColumnAlignment.LEFT.getAlignedString(6, new StringBuilder("abcdef"));
+		
+		assertEquals("Left alignment", "abcdef", alignedValue);
+	}
+	
+	@Test
+	public void doLeftAlignmentTest3() {
+		StringBuilder alignedValue = TextGridColumnAlignment.LEFT.getAlignedString(5, new StringBuilder("abcdef"));
+		
+		assertEquals("Left alignment", "ab...", alignedValue);
+	}
+	
+	@Test
+	public void doLeftAlignmentTest4() {
+		StringBuilder alignedValue = TextGridColumnAlignment.LEFT.getAlignedString(3, new StringBuilder("abcdef"));
+		
+		assertEquals("Left alignment", "...", alignedValue);
+	}
+	
+	@Test
+	public void doRightAlignmentTest() {
+		StringBuilder alignedValue = TextGridColumnAlignment.RIGHT.getAlignedString(10, new StringBuilder("abcdef"));
+		
+		assertEquals("Right alignment", "    abcdef", alignedValue);
+	}
+	
+	@Test
+	public void doRightAlignmentTest2() {
+		StringBuilder alignedValue = TextGridColumnAlignment.RIGHT.getAlignedString(6, new StringBuilder("abcdef"));
+		
+		assertEquals("Right alignment", "abcdef", alignedValue);
+	}
+	
+	@Test
+	public void doRightAlignmentTest3() {
+		StringBuilder alignedValue = TextGridColumnAlignment.RIGHT.getAlignedString(5, new StringBuilder("abcdef"));
+		
+		assertEquals("Right alignment", "...ef", alignedValue);
+	}
+	
+	@Test
+	public void doRightAlignmentTest4() {
+		StringBuilder alignedValue = TextGridColumnAlignment.RIGHT.getAlignedString(3, new StringBuilder("abcdef"));
+		
+		assertEquals("Right alignment", "...", alignedValue);
+	}
+
+	@Test
+	public void doCenterAlignmentTest() {
+		StringBuilder alignedValue = TextGridColumnAlignment.CENTER.getAlignedString(11, new StringBuilder("abcdef"));
+		
+		// O espaço extra deve ficar a direita
+		assertEquals("Center alignment", "  abcdef   ", alignedValue);
+	}
+	
+	@Test
+	public void doCenterAlignmentTest2() {
+		StringBuilder alignedValue = TextGridColumnAlignment.CENTER.getAlignedString(6, new StringBuilder("abcdef"));
+		
+		assertEquals("Center alignment", "abcdef", alignedValue);
+	}
+	
+	@Test
+	public void doCenterAlignmentTest3() {
+		StringBuilder alignedValue = TextGridColumnAlignment.CENTER.getAlignedString(5, new StringBuilder("abcdef"));
+		
+		assertEquals("Center alignment", "ab...", alignedValue);
+	}
+	
+	@Test
+	public void doCenterAlignmentTest4() {
+		StringBuilder alignedValue = TextGridColumnAlignment.CENTER.getAlignedString(3, new StringBuilder("abcdef"));
+		
+		assertEquals("Center alignment", "...", alignedValue);
+	}
+}
