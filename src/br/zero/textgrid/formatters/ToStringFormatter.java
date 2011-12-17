5212972f46ed633d404d2cb581293c55fc8b0997
rmonico <rmonico1@gmail.com>
2011-12-16 23:13:16 -0200

    Novo construtor

--- src/br/zero/textgrid/formatters/ToStringFormatter.java
+++ src/br/zero/textgrid/formatters/ToStringFormatter.java
@@ -4,6 +4,16 @@ import br.zero.textgrid.TextGridException;
 
 public class ToStringFormatter extends AbstractFormatter {
 
+	public ToStringFormatter() {
+		
+	}
+	
+	public ToStringFormatter(String nullString) {
+		this();
+		
+		setNullString(new StringBuilder(nullString));
+	}
+	
 	@Override
 	public StringBuilder parse(Object cellValue) throws TextGridException {
 		if (cellValue == null) {
