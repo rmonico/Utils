3c84cd00141b8e9044d0f776691324b327b19609
rmonico <rmonico1@gmail.com>
2012-09-12 07:05:19 -0300

    Novo TODO

--- src/br/zero/tinycontroller/Action.java
+++ src/br/zero/tinycontroller/Action.java
@@ -1,6 +1,7 @@
 package br.zero.tinycontroller;
 
 // TODO Depois criar uma annotation indicando se a ação deve ir ou não para o pool
+// TODO Depois criar VoidResultAction<ParamType> public void run(ParamType param); NoParamAction<ResultType> public ResultType run(); VoidResultNoParamAction public void run()
 public interface Action<ParamType, ResultType> {
 
 	public ResultType run(ParamType param) throws Exception;
